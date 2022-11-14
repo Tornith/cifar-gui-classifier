ed769f316457baf8283f5bca455fc69f2cd767ef
Michal <485683@mail.muni.cz>
2022-11-14 16:39:00 +0100

    Simple Counter App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('counter starts at 0', () => {
+    render(<App />);
+    const counter = screen.getByText(/0/i);
+    expect(counter).toBeInTheDocument();
+});
+
+test('counter increments by 1', () => {
+    render(<App />);
+    const counter = screen.getByTestId('counter');
+    const increment = screen.getByTestId('increment');
+    fireEvent.click(increment);
+    expect(counter).toHaveTextContent('1');
+});
+
+test('counter decrements by 1', () => {
+    render(<App />);
+    const counter = screen.getByTestId('counter');
+    const decrement = screen.getByTestId('decrement');
+    fireEvent.click(decrement);
+    expect(counter).toHaveTextContent('-1');
+});
+
+test('counter resets to 0', () => {
+    render(<App />);
+    const counter = screen.getByTestId('counter');
+    const increment = screen.getByTestId('increment');
+    fireEvent.click(increment);
+    const reset = screen.getByTestId('reset');
+    fireEvent.click(reset);
+    expect(counter).toHaveTextContent('0');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(counter + 1);
+    }
+
+    const decrement = () => {
+        setCounter(counter - 1);
+    }
+
+    return (
+        <div className="app-wrapper">
+            <header>
+                <h1>Counter app</h1>
+            </header>
+            <main>
+                <div className="counter">
+                    <div className="counter__value" data-testid="counter">{counter}</div>
+                    <div className="counter__controls">
+                        <button className="counter__button" onClick={increment} data-testid="increment">+</button>
+                        <button className="counter__button" onClick={() => setCounter(0)} data-testid="reset">r</button>
+                        <button className="counter__button" onClick={decrement} data-testid="decrement">-</button>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
